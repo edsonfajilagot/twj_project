e5094bb249bb02951f5faef92963acb27d760eaf
edsonfajilagot <edsonfajilagot@gmail.com>
2016-02-08 10:17:30 +0800

    done with chapter 8

--- rango/forms.py
+++ rango/forms.py
@@ -3,12 +3,14 @@ from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
 	name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+	#views = forms.IntegerField(help_text="Enter number of views", initial=0)
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 	
 	class Meta:
 		model = Category
+		#fields = ('name', 'views')
 		fields = ('name',)
 		
 class PageForm(forms.ModelForm):
@@ -18,4 +20,14 @@ class PageForm(forms.ModelForm):
 	
 	class Meta:
 		model = Page
-		exclude = ('category',)		
+		exclude = ('category',)
+		
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+			
+		return cleaned_data			
--- rango/urls.py~
+++ rango/urls.py~
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
 		url(r'^about/', views.about, name='about'),
 		url(r'^help/', views.help, name='help'),
 		url(r'^add_category/$', views.add_category, name='add_category'),
+		url(r'^add_page/(?P<category_name_slug>[\w\-]+)/$', views.add_page, name='add_page'),
 		url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),		
 )
