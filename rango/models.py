6c34578430b10d146a1ef350113f908ba19231f2
edsonfajilagot <edsonfajilagot@gmail.com>
2016-02-08 11:17:34 +0800

    chapter 9 user auth

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
@@ -23,3 +24,12 @@ class Page(models.Model):
 	
 	def __unicode__(self):
 		return self.title	
+		
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	
+	def __unicode__(self):
+		return self.user.username		
